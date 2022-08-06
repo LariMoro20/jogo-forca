7e518d565d250a37ea28d75fe62138b16ac796e6
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-08-06 00:59:01 -0300

    [UPDATE] Add game functions

---
+++ src/components/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="select__word items-center justify-center flex column">
+    <img src="/images/logo.png" class="hangman__game-logo" />
+    <p>{{ subtitle }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+  props: {
+    title: {
+      type: [String],
+      default: "",
+    },
+    subtitle: {
+      type: [String],
+      default: "",
+    },
+  },
+  setup(props) {},
+});
+</script>
+<style scoped>
+.hangman__game-logo {
+  width: 300px;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,7 @@
 <template>
   <q-page class="flex flex-center hangman__game" v-if="page == 1">
     <div class="select__word items-center justify-center flex column">
-      <div class="text-h3 q-pa-md">Jogo da Forca</div>
-      <p>Escolha uma palavra para começar</p>
+      <Header subtitle="Escolha uma palavra para começar" />
       <q-input
         class="full-width"
         outlined
@@ -11,6 +10,7 @@
         placeholder="Digite aqui a palavra"
       />
       <q-btn
+        :disable="!word.length"
         color="primary"
         class="q-ma-md full-width"
         label="Próximo"
@@ -20,8 +20,7 @@
   </q-page>
   <q-page class="flex flex-center hangman__game" v-if="page == 2">
     <div class="select__helpword items-center justify-center flex column">
-      <div class="text-h3 q-pa-md">Jogo da Forca</div>
-      <p>Agora, deixe uma pequena dica sobre ela</p>
+      <Header subtitle="Agora, deixe uma pequena dica sobre ela" />
       <q-input
         class="full-width"
         outlined
@@ -30,6 +29,7 @@
         placeholder="Digite aqui uma dica"
       />
       <q-btn
+        :disable="!word_help.length"
         color="primary"
         class="q-ma-md full-width"
         label="Próximo"
@@ -39,13 +39,12 @@
   </q-page>
   <q-page class="flex flex-center hangman__game" v-if="page == 3">
     <div class="hangman__game-start items-center justify-center flex column">
-      <div class="text-h3 q-pa-md">Jogo da Forca</div>
-      <p>Feito, vamos ao jogo!</p>
+      <Header subtitle="Feito, vamos ao jogo!" />
       <q-btn
         color="primary"
         class="q-ma-md full-width"
         label="Começar"
-        @click="page = 4"
+        @click="startGame"
       />
     </div>
   </q-page>
@@ -55,10 +54,14 @@
         class="hangman__game-image"
         :src="'images/game/' + errors + '.png'"
       />
+      <div class="text-center">
+        <h4 class="q-pa-none q-ma-none">{{ answer.join(" ") }}</h4>
+        <p class="q-pa-none q-ma-none"><b>Dica:</b> {{ word_help }}</p>
+      </div>
       <div class="hangman__game-keyboard flex">
         <q-btn
           color="secondary"
-          @click="errors < 6 ? errors++ : ''"
+          @click="verifyLetter(letra)"
           class="hangman__game-keyboard-key"
           v-for="(letra, key) in 'abcçdefghijklmnopqrstuvwxyz'"
           :key="key"
@@ -70,6 +73,25 @@
         v-if="errors === 6"
         color="primary"
         class="q-ma-md"
+        label="Game over.. jogar de novo?"
+        @click="restartGame"
+      />
+    </div>
+  </q-page>
+  <q-page class="flex flex-center hangman__game-sucess-page" v-if="page == 5">
+    <div
+      class="hangman__game-sucess q-pa-lg items-center justify-center flex column"
+    >
+      <img src="/images/emoji-party.gif" class="hangman__game-sucess-image" />
+      <div class="text-h3 q-pa-md">Parabéns!</div>
+      <p class="q-pa-none q-ma-none">A palavra era</p>
+      <h6 class="q-pa-none q-ma-none">{{ word }}</h6>
+      <p class="q-pa-none q-ma-none">
+        Você conseguiu descobrir a palavra secreta. Que tal jogar de novo?
+      </p>
+      <q-btn
+        color="primary"
+        class="q-ma-md"
         label="Jogar de novo"
         @click="restartGame"
       />
@@ -78,15 +100,23 @@
 </template>
 
 <script>
+import Header from "src/components/header.vue";
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "PageIndex",
+  components: { Header },
   setup() {
-    const page = ref(4);
+    const page = ref(5);
     const word = ref("");
     const word_help = ref("");
     const errors = ref(0);
+    const answer = ref("");
+
+    const startGame = () => {
+      answer.value = Array(word.value.length).fill("_");
+      page.value = 4;
+    };
 
     const restartGame = () => {
       word.value = "";
@@ -94,11 +124,23 @@ export default defineComponent({
       errors.value = 0;
       page.value = 1;
     };
-    const verifyKey = (key) => {
-      if (word.value.toLowerCase().indexOf(key.toLowerCase()) >= 0) {
+
+    const verifyLetter = (letter) => {
+      let letter_position = word.value
+        .toLowerCase()
+        .indexOf(letter.toLowerCase());
+      if (letter_position >= 0 && errors.value < 6) {
+        [...word.value].forEach((value, key) => {
+          if (value === letter) answer.value[key] = value;
+        });
+        verifyWinner();
       } else {
-        errors.value++;
+        errors.value < 6 ? errors.value++ : "";
       }
+    };
+
+    const verifyWinner = () => {
+      word.value === answer.value.join("") ? (page.value = 5) : "";
     };
 
     return {
@@ -106,13 +148,19 @@ export default defineComponent({
       word,
       word_help,
       errors,
+      answer,
+      startGame,
       restartGame,
-      verifyKey,
+      verifyLetter,
+      verifyWinner,
     };
   },
 });
 </script>
 <style scoped>
+.hangman__game-logo {
+  width: 300px;
+}
 .hangman__game-image {
   width: 300px;
 }
@@ -122,5 +170,15 @@ export default defineComponent({
 }
 .hangman__game-keyboard-key {
   margin: 5px;
+}
+.hangman__game-sucess-page {
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.hangman__game-sucess {
+  background-color: #ffffffeb;
+}
+.hangman__game-sucess-image {
+  width: 170px;
 }
 </style>
